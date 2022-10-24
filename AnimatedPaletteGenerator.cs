93168a6eb86ebf28db55654b332538897f38c845
Aaron Landwehr <snaphat@gmail.com>
2022-10-24 04:40:22 -0400

    Fix remaining bugs

--- AnimatedPaletteGenerator.cs
+++ AnimatedPaletteGenerator.cs
@@ -7,10 +7,11 @@ using System.Linq;
 
 public class AnimatedPaletteGenerator
 {
-    const int AnimationCount = 3;
-    const int Stride = AnimationCount;
-    const float minSpeed = 2f;
-    const float maxSpeed = minSpeed;
+    const int AnimationCount = 3;       // how many frames per animated tile
+    const int Stride = 4;               // how many tiles between each animation segment
+    const int cellSize = 24;            // cell size in pixels
+    const float minSpeed = 2f;          // speed where 1f = 1s, 2f = 0.5s, etc.
+    const float maxSpeed = minSpeed;    // speed where 1f = 1s, 2f = 0.5s, etc.
 
     [MenuItem("Assets/Create/2D/Tile Palette/Animated", priority = 0)]
     private static void Generate(MenuCommand menuCommand)
@@ -28,15 +29,6 @@ public class AnimatedPaletteGenerator
             var srcPath = AssetDatabase.GetAssetPath(item);
             var dstPath = Path.GetDirectoryName(srcPath);
             var file = Path.GetFileNameWithoutExtension(srcPath);
-
-            // create Palette
-            GameObject tilePalette = GridPaletteUtility.CreateNewPalette(dstPath, file + ".Palette",
-            GridLayout.CellLayout.Rectangle,
-            GridPalette.CellSizing.Automatic, new(0.16f, 0.16f, 0.16f), GridLayout.CellSwizzle.XYZ);
-            Tilemap paletteTilemap = tilePalette.GetComponentInChildren<Tilemap>();
-
-            // Generate a folder for the tiles and grab the path Unity actually made
-            dstPath = AssetDatabase.GUIDToAssetPath(AssetDatabase.CreateFolder(dstPath, file + ".AnimatedTiles"));
 
             // Load asset, order, and filter sprites
             var data = AssetDatabase.LoadAllAssetsAtPath(srcPath);
@@ -57,6 +49,15 @@ public class AnimatedPaletteGenerator
                 return;
             }
 
+            // create Palette
+            GameObject tilePalette = GridPaletteUtility.CreateNewPalette(dstPath, file + ".Palette",
+            GridLayout.CellLayout.Rectangle,
+            GridPalette.CellSizing.Automatic, new(cellSize/100.0f, cellSize/100.0f, cellSize/100.0f), GridLayout.CellSwizzle.XYZ);
+            Tilemap paletteTilemap = tilePalette.GetComponentInChildren<Tilemap>();
+
+            // Generate a folder for the tiles and grab the path Unity actually made
+            dstPath = AssetDatabase.GUIDToAssetPath(AssetDatabase.CreateFolder(dstPath, file + ".AnimatedTiles"));
+
             // Pregenerate each animated tile and add as an AnimatedTile asset
             var totalTiles = data.Length / AnimationCount;
             var animatedTile = new AnimatedTile[totalTiles];
@@ -66,16 +67,16 @@ public class AnimatedPaletteGenerator
                 animatedTile[j].m_AnimatedSprites = new Sprite[AnimationCount];
                 animatedTile[j].m_MinSpeed = minSpeed;
                 animatedTile[j].m_MaxSpeed = maxSpeed;
-                //paletteTilemap.SetTile(new Vector3Int(j % Stride, -j / Stride, 0), animatedTile[j]);
-                paletteTilemap.SetTile(new Vector3Int(j % (width / (Stride * 16)), -j / (width / (Stride * 16)), 0), animatedTile[j]);
+                int chunk = width / (AnimationCount * cellSize);
+                paletteTilemap.SetTile(new Vector3Int(j % chunk, -j / chunk, 0), animatedTile[j]);
             }
 
             // Add sprites to animated tiles at the given stride
             var count = 0;
             foreach (Sprite sprite in data)
             {
-                var k = count % Stride + Stride * (count / (Stride * Stride));
-                animatedTile[k].m_AnimatedSprites[count / Stride % Stride] = sprite;
+                var k = count % Stride + Stride * (count / (Stride * AnimationCount));
+                animatedTile[k].m_AnimatedSprites[(count / Stride) % AnimationCount] = sprite;
                 count++;
             }
 
@@ -86,8 +87,9 @@ public class AnimatedPaletteGenerator
 
             // Print results
             Debug.Log("<color=yellow><color=cyan>Animated Palette:</color> Created Tile Palette and <color=red>" +
-            data.Length / AnimationCount + "</color> Animated Tiles using a stride of <color=red>" + Stride +
-            "</color> with <color=red>" + AnimationCount + "</color> sprites per tile.</color>");
+            data.Length / AnimationCount + "</color> Animated Tiles of size <color=red>" + cellSize + "x" + cellSize +
+            "</color> using a stride of <color=red>" + Stride + "</color> with <color=red>" + AnimationCount
+            + "</color> sprites per tile.</color>");
         }
 
     }
